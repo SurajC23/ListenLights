4e682fc0353eeb581ffce081d541e32fca537334
Suraj.Chavan <Suraj.Chavan@ril.com>
2022-02-26 14:42:23 +0530

    DPR and GRPO Fragment Changes

--- app/src/main/java/com/listenlights/views/fragments/DPRFragment.java
+++ app/src/main/java/com/listenlights/views/fragments/DPRFragment.java
@@ -10,51 +10,15 @@ import android.view.ViewGroup;
 
 import com.listenlights.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DPRFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DPRFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public DPRFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DPRFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DPRFragment newInstance(String param1, String param2) {
-        DPRFragment fragment = new DPRFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
