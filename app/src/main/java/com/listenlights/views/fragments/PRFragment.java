8a58dc65baedcc858b48300312910943b4970295
Suraj.Chavan <Suraj.Chavan@ril.com>
2022-03-01 21:08:22 +0530

    PR and SO items layout added with ui

--- app/src/main/java/com/listenlights/views/fragments/PRFragment.java
+++ app/src/main/java/com/listenlights/views/fragments/PRFragment.java
@@ -2,6 +2,7 @@ package com.listenlights.views.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.listenlights.R;
 import com.listenlights.adapter.PRMainTabAdapter;
 
@@ -40,10 +42,38 @@ public class PRFragment extends Fragment {
     private void initViews(View view) {
         tabLayout = view.findViewById(R.id.mTabLayout);
         viewPager = view.findViewById(R.id.vpMain);
+        setupAdapter(tabLayout,viewPager);
+    }
 
+    private void setupAdapter(TabLayout tabLayout, ViewPager2 viewPager) {
         mainTabAdapter = new PRMainTabAdapter(getActivity());
         viewPager.setAdapter(mainTabAdapter);
 
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position == 0) tab.setText("Purchase Request");
+                if (position == 1) tab.setText("SO Items");
+            }
+        }).attach();
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.setCurrentItem(0);
     }
 }
